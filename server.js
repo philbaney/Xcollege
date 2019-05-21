e98be127af81c4eca5131fba7511ef60684c8dde
Administrator <phil@r-t-s.co.uk>
2019-05-21 14:42:47 +0100

    added models and controllers

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const Hapi = require('hapi');
 const Hoek = require('hoek');
 const Settings = require('./settings');
 const Routes = require('./lib/routes');
-
+const Models = require('./lib/models/');
 const server = new Hapi.Server({  
   host: 'localhost',
   port: 3000
 })
 
 server.route(Routes);
-
-server.start((err) => {
-  Hoek.assert(!err, err);
-
-  console.log(`Server running at: ${server.info.uri}`);
+Models.sequelize.sync().then(() => {
+	server.start((err) => {
+		Hoek.assert(!err, err);
+		console.log(`Server running at: ${server.info.uri}`);
+	});
 });
